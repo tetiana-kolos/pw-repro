149dd6692442385dcccab5da1317a4834cafc453
tetiana-kolos <t.kolos@studytube.nl>
2025-03-18 20:51:20 +0200

    updated README

--- ClockBug.spec.ts
+++ ClockBug.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test(`Reproduction of the 'Clock' issue`, async ({ page }) => {
-  await page.goto('https://955aab.studytube-staging21.nl/');
+  await page.goto('https://7d7377.studytube-staging21.nl/');
   await page.fill('input[name="user[email]"]', 'repro@test.com');
-  await page.fill('input[name="user[password]"]', 'playwright007!');
+  await page.fill('input[name="user[password]"]', 'playwright777!');
   await page.click('button[type="submit"]');
-  await expect(page).toHaveURL(/my-learning/);
-  await page.clock.setFixedTime(new Date('2025-01-01'));
-  await page.goto('https://955aab.studytube-staging21.nl/my-learning');
+  await expect(page).toHaveURL('https://7d7377.studytube-staging21.nl/my-learning');
+  await page.clock.setFixedTime(new Date('2026-01-01'));
+  await page.goto('https://7d7377.studytube-staging21.nl/my-learning');
   await expect(page.locator('stu-unified-header')).toBeVisible();
-  await expect(page.locator('stu-personal-budget-amount').nth(1)).not.toHaveClass(
-    'profile-widget__button-text--crossed',
-  );
+  await expect(page.locator('.profile-widget__button-text')).toHaveClass(/crossed/);
 });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pw-repro
+# How to reproduce the issue
+1. Clone the repository
+2. Run the test in the file ClockBug.spec.ts
+
+**Actual result**
+The test hangs and never finishes due to .clock()
+
+**Expected result**
+The test should pass or fail on timeout
