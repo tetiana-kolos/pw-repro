36f9e432b5e6cc4180e50f286c69ca9ce81dab95
tetiana-kolos <t.kolos@studytube.nl>
2025-03-25 15:27:38 +0200

    added description for the reproduction

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # How to reproduce the issue
 1. Clone the repository
-2. Run the test in the file ClockBug.spec.ts
+2. Open the file ClockBug.spec.ts
+
+_Test number one:_
+1. Run the Test ONE in the debug mode (--debug)
+2. Try to inspect elements during page.pause()
+
+_Test number two_
+1. Run Test TWO in the ui mode
 
 **Actual result**
-The test hangs and never finishes due to .clock()
+_For Test ONE:_
+1. The browser freezes, and it's NOT possible to inspect elements after page.pause()
+2. The test does NOT fail, but hangs indefinitely
+
+_For Test TWO:_
+1. The test does NOT fail on timeout, but hangs indefinitely
 
 **Expected result**
-The test should pass or fail on timeout
+1. _For Test number one:_ the test is PASSED
+2. _For Test number two:_ the test is FAILED on timeout
--- tests/ClockBug.spec.ts
+++ tests/ClockBug.spec.ts
@@ -1,14 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test.only(`Reproduction of the 'Clock' issue`, async ({ page }) => {
-  await page.goto('https://7d7377.studytube-staging21.nl/');
-  await page.fill('input[name="user[email]"]', 'repro@test.com');
-  await page.fill('input[name="user[password]"]', 'playwright777!');
-  await page.click('button[type="submit"]');
-  await expect(page).toHaveURL('https://7d7377.studytube-staging21.nl/my-learning');
-  await page.clock.setFixedTime(new Date('2026-01-01'));
-  await page.goto('https://7d7377.studytube-staging21.nl/my-learning');
-  await expect(page.locator('stu-unified-header')).toBeVisible();
-  await page.pause()
-  await expect(page.locator('.profile-widget__button-text')).toHaveClass(/crossed/);
+test.describe(`Reproduction of the 'Clock' issue`, () => {
+
+  test(`Test ONE - run this test in DEBUG MODE:
+  Check browser freeze + infinite timeout when interacting with the browser after page.pause`, async ({ page }) => {
+    await page.goto('https://7d7377.studytube-staging21.nl/');
+    await page.fill('input[name="user[email]"]', 'repro@test.com');
+    await page.fill('input[name="user[password]"]', 'playwright777!');
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL('https://7d7377.studytube-staging21.nl/my-learning');
+    await page.clock.setFixedTime(new Date('2026-01-01'));
+    await page.goto('https://7d7377.studytube-staging21.nl/my-learning');
+    await expect(page.locator('stu-unified-header')).toBeVisible();
+    await page.pause()
+    await expect(page.locator('.profile-widget__button-text')).toHaveClass(/crossed/);
+  });
+
+  test(`Test TWO - run this test in UI MODE:
+  Check browser freeze + infinite timeout when the assertion is unsuccessful`, async ({ page }) => {
+    await page.goto('https://7d7377.studytube-staging21.nl/');
+    await page.fill('input[name="user[email]"]', 'repro@test.com');
+    await page.fill('input[name="user[password]"]', 'playwright777!');
+    await page.click('button[type="submit"]');
+    await expect(page).toHaveURL('https://7d7377.studytube-staging21.nl/my-learning');
+    await page.clock.setFixedTime(new Date('2026-01-01'));
+    await page.goto('https://7d7377.studytube-staging21.nl/my-learning');
+    await expect(page.locator('stu-unified-header')).toBeVisible();
+    await expect(page.locator('.profile-widget__button-text')).toHaveClass(/non-existent-class/);
+  });
 });
